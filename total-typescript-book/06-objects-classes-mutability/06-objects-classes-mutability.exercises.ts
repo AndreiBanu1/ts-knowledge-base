b8acaaf1575b93332fd2eb7ca48745efbb175009
Andrei Cristian Banu <andreibanu97@yahoo.com>
2026-07-23 19:09:54 +0300

    chapter 6

--- total-typescript-book/06-objects-classes-mutability/06-objects-classes-mutability.exercises.ts
+++ total-typescript-book/06-objects-classes-mutability/06-objects-classes-mutability.exercises.ts
@@ -1,72 +1,72 @@
 // Intersection types
 export type Album = {
-    title: string;
-    artist: string;
-    releaseYear: number;
-    genre?: string;
-};
+  title: string
+  artist: string
+  releaseYear: number
+  genre?: string
+}
 
 export type SalesData = {
-    unitsSold: number;
-    revenue: number;
-};
+  unitsSold: number
+  revenue: number
+}
 
-export type AlbumSales = Album & SalesData;
+export type AlbumSales = Album & SalesData
 
 export const wishYouWereHereSales: AlbumSales = {
-    title: "Wish You Were Here",
-    artist: "Pink Floyd",
-    releaseYear: 1975,
-    unitsSold: 13000000,
-    revenue: 65000000,
-};
+  title: 'Wish You Were Here',
+  artist: 'Pink Floyd',
+  releaseYear: 1975,
+  unitsSold: 13000000,
+  revenue: 65000000,
+}
 
 // Combining incompatible types results in never
 type User1 = {
-    age: number;
-};
+  age: number
+}
 
 type User2 = {
-    age: string;
-};
+  age: string
+}
 
-type User3 = User1 & User2; // never
+type User3 = User1 & User2 // never
 
 // Interfaces have the ability to extend other types
 export interface Albom {
-    title: string;
-    artist: string;
-    releaseYear: number;
+  title: string
+  artist: string
+  releaseYear: number
 }
 
 export interface StudioAlbum extends Albom {
-    studio: string;
-    producer: string;
+  studio: string
+  producer: string
 }
 
 export interface LiveAlbum extends Albom {
-    concertVenue: string;
-    concertDate: Date;
+  concertVenue: string
+  concertDate: Date
 }
 
 export const americanBeauty: StudioAlbum = {
-    title: "American Beauty",
-    artist: "Grateful Dead",
-    releaseYear: 1970,
-    studio: "Wally Heider Studios",
-    producer: "Grateful Dead and Stephen Barncard",
-};
+  title: 'American Beauty',
+  artist: 'Grateful Dead',
+  releaseYear: 1970,
+  studio: 'Wally Heider Studios',
+  producer: 'Grateful Dead and Stephen Barncard',
+}
 
 export const oneFromTheVault: LiveAlbum = {
-    title: "One from the Vault",
-    artist: "Grateful Dead",
-    releaseYear: 1991,
-    concertVenue: "Great American Music Hall",
-    concertDate: new Date("1975-08-13"),
-};
+  title: 'One from the Vault',
+  artist: 'Grateful Dead',
+  releaseYear: 1991,
+  concertVenue: 'Great American Music Hall',
+  concertDate: new Date('1975-08-13'),
+}
 
 export interface BonusConcertEdition extends StudioAlbum, LiveAlbum {
-    numberOfDiscs: number;
+  numberOfDiscs: number
 }
 
 /**
@@ -79,93 +79,93 @@ export interface BonusConcertEdition extends StudioAlbum, LiveAlbum {
 
 // Exercise 6-1: Creating an Intersection Type
 export type User = {
-    name: string;
-    email: string;
-} & BaseEntity;
+  name: string
+  email: string
+} & BaseEntity
 
 export type Product = {
-    name: string;
-    price: number;
-} & BaseEntity;
+  name: string
+  price: number
+} & BaseEntity
 
 export type BaseEntity = {
-    id: string;
-    createdAt: Date;
+  id: string
+  createdAt: Date
 }
 
 export const product: Product = {
-    id: "dasdsa",
-    createdAt: new Date(8.64e15),
-    name: "name",
-    price: 2314,
+  id: 'dasdsa',
+  createdAt: new Date(8.64e15),
+  name: 'name',
+  price: 2314,
 }
 
 // Exercise 6-2: Extending Interfaces
 export interface IBaseEntity {
-    id: string;
-    createdAt: Date;
+  id: string
+  createdAt: Date
 }
 
 export interface IProduct extends IBaseEntity {
-    name: string;
-    price: number;
+  name: string
+  price: number
 }
 
 export interface IUser extends IBaseEntity {
-    name: string;
-    email: string;
+  name: string
+  email: string
 }
 
 // Object keys can not be added dynamically, if you don't declare index signature
 const albumAwards: {
-    [index: string]: boolean
-} = {};
+  [index: string]: boolean
+} = {}
 
-albumAwards.Grammy = true;
-albumAwards.MercuryPrize = false;
-albumAwards.Billboard = true;
+albumAwards.Grammy = true
+albumAwards.MercuryPrize = false
+albumAwards.Billboard = true
 
 interface AlbumAwards {
-    [index: string]: boolean;
+  [index: string]: boolean
 }
 
 // Record type supports dynamic keys
-const albumRewards: Record<string, boolean> = {};
-albumRewards.Grammy = true;
+const albumRewards: Record<string, boolean> = {}
+albumRewards.Grammy = true
 
-const albumAwards2: Record<"Grammy" | "MercuryPrize" | "Billboard", boolean> = {
-    Grammy: true,
-    MercuryPrize: false,
-    Billboard: true,
-};
+const albumAwards2: Record<'Grammy' | 'MercuryPrize' | 'Billboard', boolean> = {
+  Grammy: true,
+  MercuryPrize: false,
+  Billboard: true,
+}
 
-type BaseAwards = "Grammy" | "MercuryPrize" | "Billboard";
+type BaseAwards = 'Grammy' | 'MercuryPrize' | 'Billboard'
 
 type ExtendedAlbumAwards = Record<BaseAwards, boolean> & {
-    [award: string]: boolean;
-};
+  [award: string]: boolean
+}
 
 const extendedNominations: ExtendedAlbumAwards = {
-    Grammy: true,
-    MercuryPrize: false,
-    Billboard: true, // Additional awards can be dynamically added.
-    "American Music Awards": true,
-};
+  Grammy: true,
+  MercuryPrize: false,
+  Billboard: true, // Additional awards can be dynamically added.
+  'American Music Awards': true,
+}
 
 interface IBaseAwards {
-    Grammy: boolean;
-    MercuryPrize: boolean;
-    Billboard: boolean;
+  Grammy: boolean
+  MercuryPrize: boolean
+  Billboard: boolean
 }
 
 interface IExtendedAlbumAwards extends IBaseAwards {
-    [award: string]: boolean;
+  [award: string]: boolean
 }
 
 // The Property Key Type
 type AlbumPK = {
-    [key: PropertyKey]: string;
-};
+  [key: PropertyKey]: string
+}
 
 // Exercise 6-3: Using an Index Signature for Dynamic Keys
 
@@ -185,101 +185,139 @@ type AlbumPK = {
 // } = {};
 
 // method IV:
-const scores: Record<string, number> = {};
+const scores: Record<string, number> = {}
 
-scores.math = 95;
-scores.english = 90;
-scores.science = 85;
-
+scores.math = 95
+scores.english = 90
+scores.science = 85
 
 // Exercise 6-4: Default Properties with Dynamic Keys
 interface Scores {
-    [subject: string]: number;
+  [subject: string]: number
 
-    math: number;
-    english: number;
-    science: number;
+  math: number
+  english: number
+  science: number
 }
 
 const scores2: Scores = {
-    math: 95,
-    english: 90,
-    science: 90,
-};
+  math: 95,
+  english: 90,
+  science: 90,
+}
 
-scores.athletics = 100;
-scores.french = 75;
-scores.spanish = 70;
+scores.athletics = 100
+scores.french = 75
+scores.spanish = 70
 
 // Exercise 6-5: Restricting Object Keys with Records
-export type Environment = "development" | "production" | "staging";
+export type Environment = 'development' | 'production' | 'staging'
 
 export type Configurations = Record<
-    Environment,
-    {
-        apiBaseUrl: string;
-        timeout: number;
-    }
->;
+  Environment,
+  {
+    apiBaseUrl: string
+    timeout: number
+  }
+>
 
 export const configurations: Configurations = {
-    development: {
-        apiBaseUrl: "http://localhost:8080",
-        timeout: 5000,
-    },
-    production: {
-        apiBaseUrl: "https://api.example.com",
-        timeout: 10000,
-    },
-    staging: {
-        apiBaseUrl: "https://staging.example.com",
-        timeout: 8000,
-    },
-    // @ts-expect-error // red squiggly line under @ts-expect-error
-    notAllowed: {
-        apiBaseUrl: "https://staging.example.com",
-        timeout: 8000,
-    },
-};
+  development: {
+    apiBaseUrl: 'http://localhost:8080',
+    timeout: 5000,
+  },
+  production: {
+    apiBaseUrl: 'https://api.example.com',
+    timeout: 10000,
+  },
+  staging: {
+    apiBaseUrl: 'https://staging.example.com',
+    timeout: 8000,
+  },
+  // @ts-expect-error // red squiggly line under @ts-expect-error
+  notAllowed: {
+    apiBaseUrl: 'https://staging.example.com',
+    timeout: 8000,
+  },
+}
 
 // Exercise 6-6: Dynamic Key Support
 export const hasKey = (obj: object, key: PropertyKey) => {
-    return obj.hasOwnProperty(key);
-};
+  return obj.hasOwnProperty(key)
+}
 
 // The Partial Type: create a new object type from an existing one, except all of its properties are optional.
-type PartialAlbum = Partial<Album>;
+type PartialAlbum = Partial<Album>
 
 const updateAlbum = (album: PartialAlbum) => {
-    // . . .
-};
+  // . . .
+}
 
-updateAlbum({title: "Geogaddi", artist: "Boards of Canada"});
+updateAlbum({ title: 'Geogaddi', artist: 'Boards of Canada' })
 
 // The Required Type: ll the properties of a given object type are required.
-type RequiredAlbum = Required<Album>;
+type RequiredAlbum = Required<Album>
 
 const doubleCup: RequiredAlbum = {
-    title: "Double Cup",
-    artist: "DJ Rashad",
-    releaseYear: 2013,
-    genre: "Juke",
-};
+  title: 'Double Cup',
+  artist: 'DJ Rashad',
+  releaseYear: 2013,
+  genre: 'Juke',
+}
 
-type AlbumData = Pick<Album, "title" | "artist">;
+type AlbumData = Pick<Album, 'title' | 'artist'>
 
-type AlbumDataWithOmit = Omit<Album, "id" | "releaseYear" | "genre">;;
+type AlbumDataWithOmit = Omit<Album, 'id' | 'releaseYear' | 'genre'>
 
 // Exercise 6-7: Expecting Certain Properties
 interface User67 {
-  id: string;
-  name: string;
-  email: string;
-  role: string;
+  id: string
+  name: string
+  email: string
+  role: string
 }
 
 export const fetchUser = async (): Promise<Pick<User67, 'name' | 'email'>> => {
-  const response = await fetch("/api/user");
-  const user = await response.json();
-  return user;
-};
+  const response = await fetch('/api/user')
+  const user = await response.json()
+  return user
+}
+
+//Exercise 6-8: Updating a Product
+interface Product68 {
+  id: number
+  name: string
+  price: number
+  description: string
+}
+
+const updateProduct = (id: number, productInfo: Partial<Omit<Product68, 'id'>>) => {
+  // Do something with the productInfo.
+}
+
+updateProduct(1, {
+  // red squiggly line under the entire object
+  name: 'Book',
+})
+
+updateProduct(1, {
+  // red squiggly line under the entire object
+  price: 12.99,
+})
+
+updateProduct(1, {
+  // red squiggly line under the entire object
+  description: 'A book about Dragons',
+})
+
+updateProduct(1, {
+  // red squiggly line under the entire object
+  name: 'Book',
+  price: 12.99,
+})
+
+updateProduct(1, {
+  // red squiggly line under the entire object
+  name: 'Book',
+  description: 'A book about Dragons',
+})
